41d46e1dba74021ee75f47009b7ec3dda8a2004c
Ricardo Alves <ralvespiedade@gmail.com>
2023-03-09 21:24:27 -0300

    avançando com a logica

--- javascript/jogo.js
+++ javascript/jogo.js
@@ -2,7 +2,7 @@
 
 let regressivo = document.querySelector('.regressivo')
 let respostaUsuario = document.querySelector('.resposta')
-console.log(respostaUsuario.addEventListener(click, novaJogada))
+respostaUsuario.addEventListener('keypress', verificacaoResposta)
 let feedbackJogada = document.querySelector('.respostaCorreta')
 let num1 = parseInt(Math.random()*10)
 let num2 = parseInt(Math.random()*10)
@@ -10,20 +10,29 @@ let jogada = `${num1} x ${num2} = `
 let respostaCorreta = num1*num2
 regressivo.innerHTML = jogada 
 
+console.log(`num1 é igual a ${num1}`)
+console.log(`num2 é igual a ${num2}`)
+console.log(`num1 X num2 = ${respostaCorreta}`)
+console.log(`Aguardando a resposta do usuário.`)
+
+function verificacaoResposta(evento) {
+    console.log(evento.key)
+    if (evento.key == 'Enter') {
+        novaJogada()
+    }
+}
+
 
 function novaJogada(){
     let num1 = parseInt(Math.random()*10)
     let num2 = parseInt(Math.random()*10)
-    
-    let respostaCorreta = num1 * num2
     let jogada = `${num1} x ${num2} = ` 
+    
+    
     regressivo.innerHTML = jogada
+    respostaUsuario.value = ''
     
     
-    
-    console.log(`num1 é igual a ${num1}`)
-    console.log(`num2 é igual a ${num2}`)
-    console.log(`num1 X num2 = ${respostaCorreta}`)
-    console.log(`A resposta do usuário foi ${respostaUsuario}`)
 }
 
+console.log(respostaUsuario.value)
