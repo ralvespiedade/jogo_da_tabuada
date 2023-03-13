975e4aa7deeb8f5e64b8418151208e5e85808e17
Ricardo Alves <ralvespiedade@gmail.com>
2023-03-13 12:20:01 -0300

    implementanto contagem de pontos

--- javascript/jogo.js
+++ javascript/jogo.js
@@ -1,38 +1,82 @@
 //1ª jogada
 
-let regressivo = document.querySelector('.regressivo')
+let multiplicacao = document.querySelector('.multiplicacao')
 let respostaUsuario = document.querySelector('.resposta')
 respostaUsuario.addEventListener('keypress', verificacaoResposta)
-let feedbackJogada = document.querySelector('.respostaCorreta')
+let feedbackJogada = document.querySelector('.feedbackJogada')
 let num1 = parseInt(Math.random()*10)
 let num2 = parseInt(Math.random()*10)
 let jogada = `${num1} x ${num2} = ` 
 let respostaCorreta = num1*num2
-regressivo.innerHTML = jogada 
+let quantidadeJogadas = 1
+let pontuacao = 0
+multiplicacao.innerHTML = jogada 
 
-console.log(`num1 é igual a ${num1}`)
-console.log(`num2 é igual a ${num2}`)
-console.log(`num1 X num2 = ${respostaCorreta}`)
+console.log('--------------------------------------')
+console.log(`Jogada Nº ${quantidadeJogadas} iniciada`)
+console.log(`${num1} X ${num2} = ${respostaCorreta}`)
 console.log(`Aguardando a resposta do usuário.`)
 
+
 function verificacaoResposta(evento) {
-    console.log(evento.key)
+    //console.log(evento.key)
     if (evento.key == 'Enter') {
+        if (parseInt(respostaUsuario.value) == respostaCorreta) {
+            feedbackJogada.classList.add('quandoCorreto')
+            setTimeout(function() {
+                feedbackJogada.innerHTML = ''
+            }, 1000)
+            feedbackJogada.innerHTML = 'Resposta correta!!'
+            console.log(`Resposta usuário ${respostaUsuario.value} está correta!!`)
+            console.log(`Fim da jogada Nº ${quantidadeJogadas}`)
+            console.log('**************************************')
+            pontuacao++
+        } else {
+            feedbackJogada.classList.add('quandoIncorreto')
+            feedbackJogada.innerHTML = 'Resposta incorreta!!'
+            setTimeout(function() {
+                feedbackJogada.innerHTML = ''
+                feedbackJogada.classList.remove('quandoIncorreto')
+            }, 1000)
+            console.log(`Resposta usuário ${respostaUsuario.value} está incorreta!!`)
+            console.log(`Fim da jogada Nº ${quantidadeJogadas}`)
+            console.log('**************************************')
+            pontuacao--
+        }
+        
+        console.log(`Pontuação atual: ${pontuacao}`)
+        
         novaJogada()
     }
 }
 
-
+//2ª jogada em diante
 function novaJogada(){
-    let num1 = parseInt(Math.random()*10)
-    let num2 = parseInt(Math.random()*10)
-    let jogada = `${num1} x ${num2} = ` 
-    
-    
-    regressivo.innerHTML = jogada
-    respostaUsuario.value = ''
-    
+    if (quantidadeJogadas > 10) {
+
+        console.log('Game Over!')
+        console.log(`Score: ${pontuacao}`)
+
+    } else {
+
+        quantidadeJogadas++
+        console.log('--------------------------------------')
+        console.log(`Jogada Nº ${quantidadeJogadas} inicada`)
+        let num1 = parseInt(Math.random()*10)
+        let num2 = parseInt(Math.random()*10)
+        let jogada = `${num1} x ${num2} = ` 
+        let respostaCorreta = num1*num2
+        
+        
+        multiplicacao.innerHTML = jogada
+        respostaUsuario.value = ''
+        
+        console.log(`${num1} X ${num2} = ${respostaCorreta}`)
+        console.log(`Aguardando a resposta do usuário.`)
+        
+        verificacaoResposta()
+    }
     
 }
 
-console.log(respostaUsuario.value)
+
